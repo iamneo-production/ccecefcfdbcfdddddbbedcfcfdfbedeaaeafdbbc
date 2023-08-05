f04a1ecd51cf470ec290a92c84ec3109beab9a99
Iamneo <techadmin@iamneo.ai>
2023-08-05 08:15:14 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
+import Banner from './components/Banner/Banner';
 
-function App() {
+const App = () => {
+  const [showBanner, setShowBanner] = useState(false);
+  const [questionsCorrect, setQuestionsCorrect] = useState(0);
+
+  const handleClick = () => {
+    setShowBanner(true);
+  };
+
+  const handleCorrect = () => {
+    setQuestionsCorrect(questionsCorrect + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Quizz App</h1>
+      {showBanner ? (
+        <Banner questionsCorrect={questionsCorrect} />
+      ) : (
+        <Button onClick={handleClick}>Start Quiz</Button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
