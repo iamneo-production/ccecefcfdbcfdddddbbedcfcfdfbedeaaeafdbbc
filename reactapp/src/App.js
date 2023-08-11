642471627cb59f8f224a5200d3e29bdd491b3bd5
Iamneo <techadmin@iamneo.ai>
2023-08-11 15:53:02 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
+import Banner from './components/Banner/Banner';
+
+const App = () => {
+  const [showBanner, setShowBanner] = useState(false);
+  const [questionsCorrect, setQuestionsCorrect] = useState(0);
+
+  const handleClick = () => {
+    setShowBanner(true);
+  };
+
+  const handleCorrect = () => {
+    setQuestionsCorrect(questionsCorrect + 1);
+  };
+
+  return (
+    <div>
+      <h1>Quizz App</h1>
+      {showBanner ? (
+        <Banner questionsCorrect={questionsCorrect} />
+      ) : (
+        <Button onClick={handleClick}>Start Quiz</Button>
+      )}
+    </div>
+  );
+};
+
+export default App;
--- reactapp/src/components/Input/Input.js
+++ reactapp/src/components/Input/Input.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+const Input = ({ label, name, value, onChange }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="text"
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default Input;
